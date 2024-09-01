3993854730bae5c45ac28c7c164ab2974d1c37b9
lyju777 <lyju777@hanmail.net>
2024-09-01 16:03:04 +0900

    install vite

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="page">
+    TEST
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+.page {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100vh;
+
+  background-color: black;
+  color: white;
+}
+</style>
+
